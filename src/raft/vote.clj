2d476fff9a318e508bed232467a52f057b994eac
John Weaver <john@saebyn.info>
2013-06-02 12:55:25 -0700

    Adds voting source file.

---
+++ src/raft/vote.clj
@@ -0,0 +1,16 @@
+(ns raft.vote
+  (:use raft.core)
+  (:import [raft.core Raft]))
+
+
+(defprotocol IVote
+  (request-vote [raft candidate-term candidate-server last-log-index last-log-term] "Requests that this raft votes for the candidate"))
+
+
+(defn request-vote [raft candidate-term candidate-server last-log-index last-log-term]
+  {:raft raft :term candidate-term :vote-granted false})
+
+
+(extend Raft
+  IVote
+  {:request-vote request-vote})
---
+++ test/raft/vote_test.clj
@@ -0,0 +1,4 @@
+(ns raft.vote-test
+  (:use midje.sweet)
+  (:require [raft.vote :refer :all]
+            [raft.core :as core]))
