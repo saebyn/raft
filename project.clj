3053c13a58a600e1d8c112ad39c2b437caafa2b2
John Weaver <john@saebyn.info>
2013-06-01 19:13:50 -0700

    Updates dev dep to midje 1.5.1.

--- project.clj
+++ project.clj
@@ -4,4 +4,4 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.1"]]
-  :profiles {:dev {:dependencies [[midje "1.5.0"]]}})
+  :profiles {:dev {:dependencies [[midje "1.5.1"]]}})
