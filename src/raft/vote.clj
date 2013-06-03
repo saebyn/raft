b73deab985941856dcb52e474190c7f96a59a464
John Weaver <john@saebyn.info>
2013-06-02 17:05:45 -0700

    Adds update current term if old as part of vote handling.

--- src/raft/vote.clj
+++ src/raft/vote.clj
@@ -7,8 +7,15 @@
   (request-vote [raft candidate-term candidate-server last-log-index last-log-term] "Requests that this raft votes for the candidate"))
 
 
+(defn- update-term-if-newer [raft new-term]
+  (if (> new-term (:current-term raft))
+    (assoc raft :current-term new-term)
+    raft))
+
+
 (defn request-vote [raft candidate-term candidate-server last-log-index last-log-term]
-  {:raft raft :term (:current-term raft) :vote-granted false})
+  (let [raft (update-term-if-newer raft candidate-term)]
+    {:raft raft :term (:current-term raft) :vote-granted false}))
 
 
 (extend Raft
--- test/raft/vote_test.clj
+++ test/raft/vote_test.clj
@@ -10,5 +10,8 @@
                     (let [raft (->
                                  (core/create-raft ..rpc.. ..store.. ..state-machine.. [..server1.. ..server2..])
                                  (assoc :current-term 1))]
-                      (request-vote raft 0 ..server1.. ..last-index.. ..last-term..) => {:raft raft :term (:current-term raft) :vote-granted false}))))
-        
+                      (request-vote raft 0 ..server1.. ..last-index.. ..last-term..) => {:raft raft :term (:current-term raft) :vote-granted false}))
+              (fact "updates the current term if there is a newer term"
+                    (let [raft (core/create-raft ..rpc.. ..store.. ..state-machine.. [..server1.. ..server2..])
+                          new-term (inc (:current-term raft))]
+                      (request-vote raft new-term ..server1.. ..last-index.. ..last-term..) => (contains {:raft (contains {:current-term new-term}) :term new-term :vote-granted false})))))
