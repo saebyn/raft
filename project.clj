83f581948cc52aef2a0c0f2ac7c89191470cf2d2
John Weaver <john@saebyn.info>
2014-09-08 20:12:59 -0700

    Adds core.async dep, upgrades to Clojure 1.6.

--- project.clj
+++ project.clj
@@ -1,14 +1,15 @@
 (defproject raft "0.1.0-SNAPSHOT"
   :description "A Clojure library that implements the Raft consensus algorithm."
-  :url "http://example.com/FIXME"
+  :url "http://github.com/saebyn/raft"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.5.1"]
+  :dependencies [[org.clojure/clojure "1.6.0"]
                  [org.clojure/tools.logging "0.3.0"]]
   :main raft.demo.core
   :profiles {:dev {:plugins [[lein-midje "3.1.3"]]
                    :dependencies [[midje "1.6.3"]
                                   [lein-midje "3.1.3"]
+                                  [org.clojure/core.async "0.1.338.0-5c5012-alpha"]
                                   [org.clojure/tools.cli "0.3.1"]
                                   [clj-logging-config "1.9.10"]
                                   [com.taoensso/nippy "2.5.0"]
