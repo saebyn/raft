4292a53e8fa15d16deac7260d1bce736004918bc
John Weaver <john@saebyn.info>
2014-09-13 15:44:29 -0700

    Replaces futures & loop/while with go-loop in server.

--- src/raft/demo/server.clj
+++ src/raft/demo/server.clj
@@ -1,8 +1,9 @@
 (ns raft.demo.server
-  (:use clojure.tools.logging)
   (:require [raft.heartbeat :refer [heartbeat decrease-election-timeout]]
             [raft.log :refer [append-entries]]
             [raft.vote :refer [request-vote]]
+            [clojure.tools.logging :as l]
+            [clojure.core.async :refer [go-loop alts!!]]
             [zeromq [zmq :as zmq]]
             [taoensso.nippy :as nippy]))
 
@@ -47,88 +48,81 @@
 
 
 (defmethod rpc :default [& args]
-  (error "Unknown incoming RPC" args))
+  (l/error "Unknown incoming RPC" args))
 
 
 (defn- rpc-server [zmq-context this-server]
-  (info "RPC server started")
-  (future
-    (with-open [responder (doto (zmq/socket zmq-context :rep)
+  (l/info "RPC server started")
+  (with-open [responder (doto (zmq/socket zmq-context :rep)
                             (zmq/bind this-server))]
-      (while true
+      (go-loop []
         (let [bytes (zmq/receive responder)
               [command args] (nippy/thaw bytes)]
-          (debug "Got RPC" command "with args" args "as" this-server)
-          (zmq/send responder (nippy/freeze (apply rpc command args))))))
-    (info "RPC server stopped")))
+          (l/debug "Got RPC" command "with args" args "as" this-server)
+          (zmq/send responder (nippy/freeze (apply rpc command args))))
+        (recur))))
 
 
 (defn- external-service-server [this-external-server]
-  (info "External service server started")
-  (future
+  (l/info "External service server started")
+  (go-loop []
     ; just sleep for a while so that we don't stop early.
     ; TODO implement me by taking incoming messages,
     ; appending to raft log, if we're the leader.
     ; otherwise, tell the client who the leader is
     (Thread/sleep 10000000000000)
-    (info "External service server stopped")))
+    (recur)))
 
 
 (defn- heartbeat-server [broadcast-time]
-  (info "Heartbeat server started")
+  (l/info "Heartbeat server started")
   (let [timeout (:election-timeout @raft-instance)]
-    (future
-      (loop [start (System/currentTimeMillis)]
-        (let [current (System/currentTimeMillis)
-              elapsed (- current start)
-              remaining (- broadcast-time elapsed)]
-          (debug "Heartbeat"
-            {:start start
-             :current current
-             :diff elapsed
-             :broadcast-time broadcast-time
-             :remaining remaining})
-          (when (> current start)
-            ; Here (- current start) is the number of milliseconds we took in
-            ; the last iteration, which should be at least broadcast-time ms.
-            (swap! raft-instance
-                   #(-> %
-                        heartbeat
-                        (decrease-election-timeout
-                          (- current start))))
-            (debug "Back from heartbeat impl"))
+    (go-loop [start (System/currentTimeMillis)]
+      (let [current (System/currentTimeMillis)
+            elapsed (- current start)
+            remaining (- broadcast-time elapsed)]
+        (l/debug "Heartbeat"
+          {:start start
+           :current current
+           :diff elapsed
+           :broadcast-time broadcast-time
+           :remaining remaining})
+        (when (> current start)
+          ; Here (- current start) is the number of milliseconds we took in
+          ; the last iteration, which should be at least broadcast-time ms.
+          (swap! raft-instance
+                 #(-> %
+                      heartbeat
+                      (decrease-election-timeout
+                        (- current start))))
+          (l/debug "Back from heartbeat impl"))
 
-          (when (pos? remaining)
-            (debug "sleeping for" (/ remaining 2) "ms")
-            (Thread/sleep (/ remaining 2))
-            (debug "Heartbeat back from sleep"))
-          (recur current)))
-      (info "Heartbeat server stopped"))))
+        (when (pos? remaining)
+          (l/debug "sleeping for" (/ remaining 2) "ms")
+          (Thread/sleep (/ remaining 2))
+          (l/debug "Heartbeat back from sleep"))
+        (recur current)))))
 
 
 (defn- stats-server []
-  (info "Stats server started")
-  (future
-    (while true
+  (l/info "Stats server started")
+  (go-loop []
+           
       ; Just dump the contents of the raft state every 5 seconds for now
       ; TODO send stats somewhere?
-      (debug "Raft instance:" @raft-instance)
-      (Thread/sleep 5000))
-    (info "Stats server stopped")))
+      (l/debug "Raft instance:" @raft-instance)
+      (Thread/sleep 5000)))
 
 
 (defn run-server [zmq-context this-server this-external-server broadcast-time]
   ; TODO stats server optional?
-  (let [stopped (promise)
-        ; The servers to operate
-        operations {:rpc (rpc-server zmq-context this-server)
-                    :external (external-service-server this-external-server)
-                    :heartbeat (heartbeat-server broadcast-time)
-                    :stats (stats-server)}]
-    (doseq [[name op] operations]
-      (future
-        (deliver stopped [name (deref op)])))
-    ; Wait until the first component stops, then quit.
-    (debug "Exiting component returned" @stopped)
-    (info "Server stopping")
-    (System/exit 0)))
+  (l/debug
+    "Exiting component returned"
+    (alts!!
+      [(rpc-server zmq-context this-server)
+       (external-service-server this-external-server)
+       (heartbeat-server broadcast-time)
+       (stats-server)]))
+
+  (l/info "Server stopped")
+  (System/exit 0))
